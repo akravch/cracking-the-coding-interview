90e139f0afb2abacaaf400d64c36a64adff67ee0
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-10 14:38:39 +0300

    Add a solution for urlification.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 03 Urlify Spaces/UrlifySpacesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using CTCI.Ch_01_Arrays_and_Strings.Task_03_Urlify_Spaces;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_03_Urlify_Spaces
+{
+    public class UrlifySpacesTests
+    {
+        [Theory]
+        [InlineData("Mr John Smith Jr", "Mr%20John%20Smith%20Jr")]
+        public void Urlify1(string initial, string expected)
+        {
+            var solution = new UrlifySpaces();
+            var textLength = initial.Length;
+            var spacesCount = initial.Count(c => c == ' ');
+            var initialArray = new char[textLength + spacesCount * 2];
+            Array.Copy(initial.ToCharArray(), initialArray, textLength);
+
+            var actual = solution.Urlify1(initialArray, textLength);
+
+            Assert.Equal(expected, new string(actual));
+        }
+
+        [Theory]
+        [InlineData("Mr John Smith Jr", "Mr%20John%20Smith%20Jr")]
+        public void Urlify2(string initial, string expected)
+        {
+            var solution = new UrlifySpaces();
+            var textLength = initial.Length;
+            var spacesCount = initial.Count(c => c == ' ');
+            var initialArray = new char[textLength + spacesCount * 2];
+            Array.Copy(initial.ToCharArray(), initialArray, textLength);
+
+            var actual = solution.Urlify2(initialArray, textLength);
+
+            Assert.Equal(expected, new string(actual));
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 03 Urlify Spaces/UrlifySpaces.cs
@@ -0,0 +1,68 @@
+﻿namespace CTCI.Ch_01_Arrays_and_Strings.Task_03_Urlify_Spaces
+{
+    public class UrlifySpaces
+    {
+        public char[] Urlify1(char[] str, int textLength)
+        {
+            var result = new char[str.Length];
+            var resultIndex = 0;
+
+            for (var i = 0; i < textLength; i++)
+            {
+                var symbol = str[i];
+
+                if (symbol != ' ')
+                {
+                    result[resultIndex++] = symbol;
+                }
+                else
+                {
+                    result[resultIndex++] = '%';
+                    result[resultIndex++] = '2';
+                    result[resultIndex++] = '0';
+                }
+            }
+
+            return result;
+        }
+
+        public char[] Urlify2(char[] str, int textLength)
+        {
+            var spaces = 0;
+
+            foreach (var c in str)
+            {
+                if (c == ' ')
+                {
+                    spaces++;
+                }
+            }
+
+            var index = textLength - 1;
+
+            while (spaces > 0)
+            {
+                var symbol = str[index];
+                var newIndex = index + spaces * 2;
+
+                if (symbol != ' ')
+                {
+                    str[newIndex] = symbol;
+                    str[index] = ' ';
+                }
+                else
+                {
+                    str[newIndex] = '0';
+                    str[newIndex - 1] = '2';
+                    str[newIndex - 2] = '%';
+
+                    spaces--;
+                }
+
+                index--;
+            }
+
+            return str;
+        }
+    }
+}
