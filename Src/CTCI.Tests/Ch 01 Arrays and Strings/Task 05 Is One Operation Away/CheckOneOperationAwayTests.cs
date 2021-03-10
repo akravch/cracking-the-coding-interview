8ead8be32bb9c92b5eb292b90357a2eacaa554d4
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-10 15:16:51 +0300

    Add a solution for the one away problem.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 05 Is One Operation Away/CheckOneOperationAwayTests.cs
@@ -0,0 +1,25 @@
+﻿using CTCI.Ch_01_Arrays_and_Strings.Task_05_Is_One_Operation_Away;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_05_Is_One_Operation_Away
+{
+    public class CheckOneOperationAwayTests
+    {
+        [Theory]
+        [InlineData("pale", "ple", true)]
+        [InlineData("pales", "pale", true)]
+        [InlineData("pale", "bale", true)]
+        [InlineData("pale", "bake", false)]
+        [InlineData("pale", "pale", true)]
+        [InlineData("pale", "paleeeeee", false)]
+        [InlineData("p", "p", true)]
+        public void IsOneAway(string str1, string str2, bool expected)
+        {
+            var solution = new CheckOneOperationAway();
+
+            var actual = solution.IsOneAway(str1, str2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 05 Is One Operation Away/CheckOneOperationAway.cs
@@ -0,0 +1,55 @@
+﻿namespace CTCI.Ch_01_Arrays_and_Strings.Task_05_Is_One_Operation_Away
+{
+    public class CheckOneOperationAway
+    {
+        public bool IsOneAway(string str1, string str2)
+        {
+            var lengthDiff = str1.Length - str2.Length;
+
+            if (lengthDiff > 1 || lengthDiff < -1)
+            {
+                return false;
+            }
+
+
+            string longerStr;
+            string shorterStr;
+
+            if (lengthDiff > 0)
+            {
+                longerStr = str1;
+                shorterStr = str2;
+            }
+            else
+            {
+                longerStr = str2;
+                shorterStr = str1;
+            }
+
+            var oneAway = false;
+
+            for (int shorterIndex = 0, longerIndex = 0; shorterIndex < shorterStr.Length; shorterIndex++, longerIndex++)
+            {
+                var char1 = shorterStr[shorterIndex];
+                var char2 = longerStr[longerIndex];
+
+                if (char1 != char2)
+                {
+                    if (oneAway)
+                    {
+                        return false;
+                    }
+
+                    oneAway = true;
+
+                    if (lengthDiff != 0)
+                    {
+                        longerIndex++;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
