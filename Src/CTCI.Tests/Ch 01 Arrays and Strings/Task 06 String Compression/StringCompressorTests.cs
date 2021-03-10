29088a60871f2f550b73a7e20d5e4918bbd8ac3a
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-10 16:03:09 +0300

    Add a string compressor.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 06 String Compression/StringCompressorTests.cs
@@ -0,0 +1,23 @@
+﻿using CTCI.Ch_01_Arrays_and_Strings.Task_06_String_Compression;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_06_String_Compression
+{
+    public class StringCompressorTests
+    {
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("aa", "aa")]
+        [InlineData("aaa", "a3")]
+        [InlineData("aaabcc", "aaabcc")]
+        [InlineData("aaabccc", "a3b1c3")]
+        public void Compress(string input, string expected)
+        {
+            var solution = new StringCompressor();
+
+            var actual = solution.Compress(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 06 String Compression/StringCompressor.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace CTCI.Ch_01_Arrays_and_Strings.Task_06_String_Compression
+{
+    public class StringCompressor
+    {
+        public string Compress(string str)
+        {
+            var length = str.Length;
+
+            if (length < 3)
+            {
+                return str;
+            }
+
+            var previousSymbol = '\0';
+            var previousSymbolCount = 0;
+            var builder = new StringBuilder();
+
+            foreach (var symbol in str)
+            {
+                if (previousSymbol != symbol)
+                {
+                    if (previousSymbolCount > 0)
+                    {
+                        builder.Append(previousSymbolCount);
+                    }
+
+                    previousSymbol = symbol;
+                    previousSymbolCount = 1;
+
+                    builder.Append(symbol);
+                }
+                else
+                {
+                    previousSymbolCount++;
+                }
+
+                if (builder.Length >= length)
+                {
+                    break;
+                }
+            }
+
+            builder.Append(previousSymbolCount);
+
+            return builder.Length >= length
+                ? str
+                : builder.ToString();
+        }
+    }
+}
