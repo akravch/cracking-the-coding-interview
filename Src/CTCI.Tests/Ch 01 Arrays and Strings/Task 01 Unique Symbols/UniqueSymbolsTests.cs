cc9ec6c996e7fefb8cbed539eb4885b5cd124212
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-10 14:01:39 +0300

    Add a solution for unique symbols.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 01 Unique Symbols/UniqueSymbolsTests.cs
@@ -0,0 +1,22 @@
+﻿using CTCI.Ch_01_Arrays_and_Strings.Task_01_Unique_Symbols;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_01_Unique_Symbols
+{
+    public class UniqueSymbolsTests
+    {
+        [Theory]
+        [InlineData("abcdefg", true)]
+        [InlineData("a", true)]
+        [InlineData("aaaaaaa", false)]
+        [InlineData("aghjklba", false)]
+        public void IsUnique(string str, bool expected)
+        {
+            var solution = new UniqueSymbols();
+
+            var isUnique = solution.IsUnique(str);
+
+            Assert.Equal(expected, isUnique);
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 01 Unique Symbols/UniqueSymbols.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_01_Arrays_and_Strings.Task_01_Unique_Symbols
+{
+    public class UniqueSymbols
+    {
+        public bool IsUnique(string str)
+        {
+            var hashSet = new HashSet<char>();
+
+            foreach (var symbol in str)
+            {
+                if (!hashSet.Add(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
