6d75cb97e50237c5b578d96b3f4f648f3c94f84b
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-10 14:14:15 +0300

    Add a solution for permuted strings.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 02 Check Permutations/CheckPermutationsTests.cs
@@ -0,0 +1,42 @@
+﻿using CTCI.Ch_01_Arrays_and_Strings.Task_02_Check_Permutations;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_02_Check_Permutations
+{
+    public class CheckPermutationsTests
+    {
+        [Theory]
+        [InlineData("abcde", "cdbae", true)]
+        [InlineData("abcde", "cdbaa", false)]
+        [InlineData("a", "a", true)]
+        [InlineData("abcd", "abcd", true)]
+        [InlineData("qwerty", "qweqweqweqweqweqwe", false)]
+        [InlineData("aaa", "a", false)]
+        [InlineData("a", "aaa", false)]
+        public void IsPermuted1(string str1, string str2, bool expected)
+        {
+            var solution = new CheckPermutations();
+
+            var actual = solution.IsPermuted1(str1, str2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("abcde", "cdbae", true)]
+        [InlineData("abcde", "cdbaa", false)]
+        [InlineData("a", "a", true)]
+        [InlineData("abcd", "abcd", true)]
+        [InlineData("qwerty", "qweqweqweqweqweqwe", false)]
+        [InlineData("aaa", "a", false)]
+        [InlineData("a", "aaa", false)]
+        public void IsPermuted2(string str1, string str2, bool expected)
+        {
+            var solution = new CheckPermutations();
+
+            var actual = solution.IsPermuted2(str1, str2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 02 Check Permutations/CheckPermutations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CTCI.Ch_01_Arrays_and_Strings.Task_02_Check_Permutations
+{
+    public class CheckPermutations
+    {
+        public bool IsPermuted1(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            var dictionary = new Dictionary<char, int>();
+
+            foreach (var c in str1)
+            {
+                dictionary.TryGetValue(c, out var count);
+                dictionary[c] = count + 1;
+            }
+
+            foreach (var c in str2)
+            {
+                dictionary.TryGetValue(c, out var count);
+                dictionary[c] = count - 1;
+            }
+
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsPermuted2(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            var array1 = str1.ToCharArray();
+            var array2 = str2.ToCharArray();
+
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            for (var i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
