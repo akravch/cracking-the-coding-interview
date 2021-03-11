1be00683b9b7c5ba892b28dd5051aeb3e38c6c63
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-11 17:27:46 +0300

    Add a solution for zeroing matrix.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 08 Zero Row and Column/ZeroMatrixTests.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_01_Arrays_and_Strings.Task_08_Zero_Row_and_Column;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_08_Zero_Row_and_Column
+{
+    public class ZeroMatrixTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void Zero1(int[,] input, int[,] expected)
+        {
+            var solution = new ZeroMatrix();
+
+            var actual = solution.Zero1(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void Zero2(int[,] input, int[,] expected)
+        {
+            var solution = new ZeroMatrix();
+
+            var actual = solution.Zero2(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var input1 = new[,]
+            {
+                { 1 }
+            };
+            var expected1 = new[,]
+            {
+                { 1 }
+            };
+
+            var input2 = new[,]
+            {
+                { 0 }
+            };
+            var expected2 = new[,]
+            {
+                { 0 }
+            };
+
+            var input3 = new[,]
+            {
+                { 1, 0, 2 }
+            };
+            var expected3 = new[,]
+            {
+                { 0, 0, 0 }
+            };
+
+            var input4 = new[,]
+            {
+                { 1 },
+                { 0 },
+                { 2 }
+            };
+            var expected4 = new[,]
+            {
+                { 0 },
+                { 0 },
+                { 0 }
+            };
+
+            var input5 = new[,]
+            {
+                { 1, 2, 3 },
+                { 4, 0, 5 },
+                { 6, 7, 8 }
+            };
+            var expected5 = new[,]
+            {
+                { 1, 0, 3 },
+                { 0, 0, 0 },
+                { 6, 0, 8 }
+            };
+
+            var input6 = new[,]
+            {
+                { 0, 2, 3 },
+                { 4, 0, 5 },
+                { 6, 7, 8 }
+            };
+            var expected6 = new[,]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 8 }
+            };
+
+            var input7 = new[,]
+            {
+                { 1,  2,  3,  4,  5  },
+                { 6,  7,  8,  0,  10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 0,  18, 19, 20 },
+                { 21, 22, 23, 24, 0  },
+            };
+            var expected7 = new[,]
+            {
+                { 1,  0,  3,  0,  0 },
+                { 0,  0,  0,  0,  0 },
+                { 11, 0,  13, 0,  0 },
+                { 0,  0,  0,  0,  0 },
+                { 0,  0,  0,  0,  0 },
+            };
+
+            var input8 = new[,]
+            {
+                { 1,  0,  0,  4,  5  },
+                { 6,  7,  8,  9,  10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 },
+            };
+            var expected8 = new[,]
+            {
+                { 0,  0,  0,  0,  0  },
+                { 6,  0,  0,  9,  10 },
+                { 11, 0,  0,  14, 15 },
+                { 16, 0,  0,  19, 20 },
+                { 21, 0,  0,  24, 25 },
+            };
+
+            yield return new object[] { input1, expected1 };
+            yield return new object[] { input2, expected2 };
+            yield return new object[] { input3, expected3 };
+            yield return new object[] { input4, expected4 };
+            yield return new object[] { input5, expected5 };
+            yield return new object[] { input6, expected6 };
+            yield return new object[] { input7, expected7 };
+            yield return new object[] { input8, expected8 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 08 Zero Row and Column/ZeroMatrix.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_01_Arrays_and_Strings.Task_08_Zero_Row_and_Column
+{
+    public class ZeroMatrix
+    {
+        private static void ZeroRow(int[,] matrix, int row)
+        {
+            for (var col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = 0;
+            }
+        }
+
+        private static void ZeroCol(int[,] matrix, int col)
+        {
+            for (var row = 0; row < matrix.GetLength(0); row++)
+            {
+                matrix[row, col] = 0;
+            }
+        }
+
+        public int[,] Zero1(int[,] matrix)
+        {
+            var rowsToZero = new HashSet<int>();
+            var colsToZero = new HashSet<int>();
+
+            for (var row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (var col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        rowsToZero.Add(row);
+                        colsToZero.Add(col);
+                    }
+                }
+            }
+
+            foreach (var row in rowsToZero)
+            {
+                ZeroRow(matrix, row);
+            }
+
+            foreach (var col in colsToZero)
+            {
+                ZeroCol(matrix, col);
+            }
+
+            return matrix;
+        }
+
+        public int[,] Zero2(int[,] matrix)
+        {
+            var firstColHasZero = false;
+
+            for (var row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (matrix[row, 0] == 0)
+                {
+                    firstColHasZero = true;
+                    break;
+                }
+            }
+
+            var firstRowHasZero = false;
+
+            for (var col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (matrix[0, col] == 0)
+                {
+                    firstRowHasZero = true;
+                    break;
+                }
+            }
+
+            for (var row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (var col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        matrix[0, col] = 0;
+                        matrix[row, 0] = 0;
+                    }
+                }
+            }
+
+            for (var row = 1; row < matrix.GetLength(0); row++)
+            {
+                if (matrix[row, 0] == 0)
+                {
+                    ZeroRow(matrix, row);
+                }
+            }
+
+            for (var col = 1; col < matrix.GetLength(1); col++)
+            {
+                if (matrix[0, col] == 0)
+                {
+                    ZeroCol(matrix, col);
+                }
+            }
+
+            if (firstRowHasZero)
+            {
+                ZeroRow(matrix, 0);
+            }
+
+            if (firstColHasZero)
+            {
+                ZeroCol(matrix, 0);
+            }
+
+            return matrix;
+        }
+    }
+}
