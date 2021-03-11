9c0c052c4a400a506b9cf464190ca2cb82114f16
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-11 18:19:19 +0300

    Add a solution for the cyclic shift.

---
+++ Src/CTCI.Tests/Ch 01 Arrays and Strings/Task 09 Cyclic Shift/CyclicShiftTests.cs
@@ -0,0 +1,25 @@
+﻿using CTCI.Ch_01_Arrays_and_Strings.Task_09_Cyclic_Shift;
+using Xunit;
+
+namespace CTCI.Tests.Ch_01_Arrays_and_Strings.Task_09_Cyclic_Shift
+{
+    public class CyclicShiftTests
+    {
+        [Theory]
+        [InlineData("waterbottle", "erbottlewat", true)]
+        [InlineData("erbottlewat", "waterbottle", true)]
+        [InlineData("waterwattle", "erbottlewat", false)]
+        [InlineData("hello", "hello", true)]
+        [InlineData("hello", "ohell", true)]
+        [InlineData("hello", "ohelo", false)]
+        [InlineData("a", "bbbbbbbb", false)]
+        public void IsCyclicShift(string str1, string str2, bool expected)
+        {
+            var solution = new CyclicShift();
+
+            var actual = solution.IsCyclicShift(str1, str2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Src/CTCI/Ch 01 Arrays and Strings/Task 09 Cyclic Shift/CyclicShift.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CTCI.Ch_01_Arrays_and_Strings.Task_09_Cyclic_Shift
+{
+    public class CyclicShift
+    {
+        public bool IsCyclicShift(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            var doubleStr1 = str1 + str1;
+            var isSubstring = doubleStr1.IndexOf(str2, StringComparison.Ordinal) != -1;
+
+            return isSubstring;
+        }
+    }
+}
