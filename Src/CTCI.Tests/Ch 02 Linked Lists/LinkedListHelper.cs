7c0b69d5c5a9aa3e9bf5c620808d87643d98ce12
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-23 18:35:12 +0300

    Add LinkedListHelper.

---
+++ Src/CTCI.Tests/Ch 02 Linked Lists/LinkedListHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Tests.Ch_02_Linked_Lists
+{
+    public static class LinkedListHelper
+    {
+        public static CTCI.Ch_02_Linked_Lists.LinkedListNode<T> FromCollection<T>(IEnumerable<T> collection)
+        {
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<T> head = null;
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<T> previous = null;
+
+            foreach (var item in collection)
+            {
+                var current = new CTCI.Ch_02_Linked_Lists.LinkedListNode<T>(item);
+
+                if (previous != null)
+                {
+                    previous.Next = current;
+                }
+
+                previous = current;
+                head ??= current;
+            }
+
+            return head;
+        }
+
+        public static List<T> ToList<T>(CTCI.Ch_02_Linked_Lists.LinkedListNode<T> head)
+        {
+            var list = new List<T>();
+            var current = head;
+
+            while (current != null)
+            {
+                list.Add(current.Value);
+                current = current.Next;
+            }
+
+            return list;
+        }
+    }
+}
