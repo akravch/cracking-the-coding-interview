3757909911497df258ac9e8ea1f77eeb03f5effd
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-23 18:23:25 +0300

    Add LinkedListNode implementation.

---
+++ Src/CTCI/Ch 02 Linked Lists/LinkedListNode.cs
@@ -0,0 +1,31 @@
+﻿namespace CTCI.Ch_02_Linked_Lists
+{
+    public class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode(T value, LinkedListNode<T> next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+
+    public class DoubleLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public DoubleLinkedListNode<T> Previous { get; set; }
+        public DoubleLinkedListNode<T> Next { get; set; }
+
+        public DoubleLinkedListNode(
+            T value,
+            DoubleLinkedListNode<T> previous = null,
+            DoubleLinkedListNode<T> next = null)
+        {
+            Value = value;
+            Next = next;
+            Previous = previous;
+        }
+    }
+}
