b90df9f16022bcbe08d05ce2e01465a69f497b21
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-24 18:42:12 +0300

    Add a solution for the list sum.

---
+++ Src/CTCI.Tests/Ch 02 Linked Lists/Task 05 Sum Lists/SumListsTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using CTCI.Ch_02_Linked_Lists.Task_05_Sum_Lists;
+using Xunit;
+
+namespace CTCI.Tests.Ch_02_Linked_Lists.Task_05_Sum_Lists
+{
+    public class SumListsTests
+    {
+        [Theory]
+        [MemberData(nameof(GetReversedTestCases))]
+        public void SumReversed(int[] first, int[] second, int[] expected)
+        {
+            var solution = new SumLists();
+            var firstLinkedList = LinkedListHelper.FromCollection(first);
+            var secondLinkedList = LinkedListHelper.FromCollection(second);
+
+            var actual = solution.SumReversed(firstLinkedList, secondLinkedList);
+
+            Assert.Equal(expected, LinkedListHelper.ToList(actual));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetStraightTestCases))]
+        public void SumStraight(int[] first, int[] second, int[] expected)
+        {
+            var solution = new SumLists();
+            var firstLinkedList = LinkedListHelper.FromCollection(first);
+            var secondLinkedList = LinkedListHelper.FromCollection(second);
+
+            var actual = solution.SumStraight(firstLinkedList, secondLinkedList);
+
+            Assert.Equal(expected, LinkedListHelper.ToList(actual));
+        }
+
+        public static IEnumerable<object[]> GetReversedTestCases()
+        {
+            var first1 = new[] { 2 };
+            var second1 = new[] { 3 };
+            var expected1 = new[] { 5 };
+
+            var first2 = new[] { 7, 1, 6 };
+            var second2 = new[] { 5, 9, 2 };
+            var expected2 = new[] { 2, 1, 9 };
+
+            var first3 = new[] { 7, 2, 9 };
+            var second3 = new[] { 8, 2, 4 };
+            var expected3 = new[] { 5, 5, 3, 1 };
+
+            var first4 = new[] { 6, 3, 5, 1, 6, 8, 2, 0, 1, 6, 2 };
+            var second4 = new[] { 1, 6, 2, 2, 1, 8 };
+            var expected4 = new[] { 7, 9, 7, 3, 7, 6, 3, 0, 1, 6, 2 };
+
+            var first5 = new[] { 0, 0, 2 };
+            var second5 = new[] { 0, 0, 3 };
+            var expected5 = new[] { 0, 0, 5 };
+
+            var first6 = new[] { 1 };
+            var second6 = new[] { 5, 2, 7, 8 };
+            var expected6 = new[] { 6, 2, 7, 8 };
+
+            var first7 = new[] { 5, 2, 7, 8 };
+            var second7 = new[] { 1 };
+            var expected7 = new[] { 6, 2, 7, 8 };
+
+            yield return new object[] { first1, second1, expected1 };
+            yield return new object[] { first2, second2, expected2 };
+            yield return new object[] { first3, second3, expected3 };
+            yield return new object[] { first4, second4, expected4 };
+            yield return new object[] { first5, second5, expected5 };
+            yield return new object[] { first6, second6, expected6 };
+            yield return new object[] { first7, second7, expected7 };
+        }
+
+        public static IEnumerable<object[]> GetStraightTestCases()
+        {
+            foreach (var reversedTestCase in GetReversedTestCases())
+            {
+                var first = (int[]) reversedTestCase[0];
+                var second = (int[]) reversedTestCase[1];
+                var expected = (int[]) reversedTestCase[2];
+
+                Array.Reverse(first);
+                Array.Reverse(second);
+                Array.Reverse(expected);
+
+                yield return new object[] { first, second, expected };
+            }
+        }
+    }
+}
---
+++ Src/CTCI/Ch 02 Linked Lists/Task 05 Sum Lists/SumLists.cs
@@ -0,0 +1,230 @@
+﻿using System;
+
+namespace CTCI.Ch_02_Linked_Lists.Task_05_Sum_Lists
+{
+    public class SumLists
+    {
+        public LinkedListNode<int> SumReversed(LinkedListNode<int> first, LinkedListNode<int> second)
+        {
+            LinkedListNode<int> sumHead = null;
+            LinkedListNode<int> sumTail = null;
+
+            var firstCurrent = first;
+            var secondCurrent = second;
+            var carry = 0;
+
+            while (firstCurrent != null && secondCurrent != null)
+            {
+                var sum = firstCurrent.Value + secondCurrent.Value + carry;
+                carry = 0;
+
+                if (sum > 9)
+                {
+                    sum -= 10;
+                    carry = 1;
+                }
+
+                var sumNode = new LinkedListNode<int>(sum);
+
+                sumHead ??= sumNode;
+
+                if (sumTail != null)
+                {
+                    sumTail.Next = sumNode;
+                }
+
+                sumTail = sumNode;
+
+                firstCurrent = firstCurrent.Next;
+                secondCurrent = secondCurrent.Next;
+            }
+
+            LinkedListNode<int> current = null;
+
+            if (firstCurrent != null)
+            {
+                current = firstCurrent;
+            }
+            else if (secondCurrent != null)
+            {
+                current = secondCurrent;
+            }
+
+            while (current != null)
+            {
+                var sum = current.Value + carry;
+                carry = 0;
+
+                if (sum > 9)
+                {
+                    sum -= 10;
+                    carry = 1;
+                }
+
+                var sumNode = new LinkedListNode<int>(sum);
+
+                sumTail.Next = sumNode;
+                sumTail = sumNode;
+
+                current = current.Next;
+            }
+
+            if (carry != 0)
+            {
+                sumTail.Next = new LinkedListNode<int>(carry);
+            }
+
+            return sumHead;
+        }
+
+        public LinkedListNode<int> SumStraight(LinkedListNode<int> first, LinkedListNode<int> second)
+        {
+            // Align the decimals
+            var firstLength = GetLength(first);
+            var secondLength = GetLength(second);
+
+            LinkedListNode<int> longerHead;
+            LinkedListNode<int> shorterHead;
+
+            if (firstLength > secondLength)
+            {
+                longerHead = first;
+                shorterHead = second;
+            }
+            else
+            {
+                longerHead = second;
+                shorterHead = first;
+            }
+
+            // Skip the required number of decimal places
+            var decimalsToSkip = Math.Abs(firstLength - secondLength);
+
+            LinkedListNode<int> sumHead = null;
+            LinkedListNode<int> sumTail = null;
+
+            var currentLonger = longerHead;
+            var currentShorter = shorterHead;
+
+            // Start building a reversed linked list
+            LinkedListNode<ReversedPair> reversedHead = null;
+
+            for (var i = 0; i < decimalsToSkip; i++)
+            {
+                var sumNode = new LinkedListNode<int>(currentLonger.Value);
+
+                sumHead ??= sumNode;
+
+                if (sumTail != null)
+                {
+                    sumTail.Next = sumNode;
+                }
+
+                sumTail = sumNode;
+                currentLonger = currentLonger.Next;
+
+                var reversedPair = new ReversedPair(sumNode, hasCarry: false);
+                var newReversedHead = new LinkedListNode<ReversedPair>(reversedPair, reversedHead);
+                reversedHead = newReversedHead;
+            }
+
+            // Calculate a happy-path sum without carries, but mark them in the reversed list
+            while (currentLonger != null && currentShorter != null)
+            {
+                var sum = currentLonger.Value + currentShorter.Value;
+                var hasCarry = false;
+
+                if (sum > 9)
+                {
+                    sum -= 10;
+                    hasCarry = true;
+                }
+
+                var sumNode = new LinkedListNode<int>(sum);
+
+                sumHead ??= sumNode;
+
+                if (sumTail != null)
+                {
+                    sumTail.Next = sumNode;
+                }
+
+                sumTail = sumNode;
+
+                var reversedPair = new ReversedPair(sumNode, hasCarry);
+                var newReversedHead = new LinkedListNode<ReversedPair>(reversedPair, reversedHead);
+                reversedHead = newReversedHead;
+
+                currentLonger = currentLonger.Next;
+                currentShorter = currentShorter.Next;
+            }
+
+            // Apply carries using the reversed list
+            var currentReversedNode = reversedHead;
+            var applyCarry = false;
+
+            while (currentReversedNode != null)
+            {
+                var pair = currentReversedNode.Value;
+
+                if (applyCarry)
+                {
+                    var node = pair.Node;
+                    var newValue = node.Value + 1;
+
+                    if (newValue > 9)
+                    {
+                        newValue -= 10;
+                    }
+                    else
+                    {
+                        applyCarry = false;
+                    }
+
+                    node.Value = newValue;
+                }
+
+                if (pair.HasCarry)
+                {
+                    applyCarry = true;
+                }
+
+                currentReversedNode = currentReversedNode.Next;
+            }
+
+            // Add an additional decimal place at the beginning if needed
+            if (applyCarry)
+            {
+                sumHead = new LinkedListNode<int>(1, sumHead);
+            }
+
+            return sumHead;
+        }
+
+        private sealed class ReversedPair
+        {
+            public ReversedPair(LinkedListNode<int> node, bool hasCarry)
+            {
+                Node = node;
+                HasCarry = hasCarry;
+            }
+
+            public LinkedListNode<int> Node { get; }
+            public bool HasCarry { get; }
+        }
+
+        private static int GetLength(LinkedListNode<int> head)
+        {
+            var length = 0;
+            var current = head;
+
+            while (current != null)
+            {
+                length++;
+                current = current.Next;
+            }
+
+            return length;
+        }
+    }
+}
