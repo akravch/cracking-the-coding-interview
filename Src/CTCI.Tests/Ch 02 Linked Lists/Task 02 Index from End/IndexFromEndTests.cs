614ac417092339a048e8b88af98aa694a6a94d2f
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-24 11:57:11 +0300

    Add a solution for index from the end on linked list.

---
+++ Src/CTCI.Tests/Ch 02 Linked Lists/Task 02 Index from End/IndexFromEndTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_02_Linked_Lists.Task_02_Index_from_End;
+using Xunit;
+
+namespace CTCI.Tests.Ch_02_Linked_Lists.Task_02_Index_from_End
+{
+    public class IndexFromEndTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void ElementByIndexFromEnd1(int[] input, int index, int expected)
+        {
+            var solution = new IndexFromEnd();
+
+            var actual = solution.ElementByIndexFromEnd1(
+                LinkedListHelper.FromCollection(input),
+                index);
+
+            Assert.Equal(expected, actual.Value);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void ElementByIndexFromEnd2(int[] input, int index, int expected)
+        {
+            var solution = new IndexFromEnd();
+
+            var actual = solution.ElementByIndexFromEnd2(
+                LinkedListHelper.FromCollection(input),
+                index);
+
+            Assert.Equal(expected, actual.Value);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var input1 = new[] { 1, 2, 3 };
+            var index1 = 1;
+            var expected1 = 2;
+
+            var input2 = new[] { 1, 2, 3, 3, 3, 3, 3, 3 };
+            var index2 = 0;
+            var expected2 = 3;
+
+            var input3 = new[] { 1 };
+            var index3 = 0;
+            var expected3 = 1;
+
+            var input4 = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var index4 = 6;
+            var expected4 = 1;
+
+            var input5 = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var index5 = 3;
+            var expected5 = 4;
+
+            yield return new object[] { input1, index1, expected1 };
+            yield return new object[] { input2, index2, expected2 };
+            yield return new object[] { input3, index3, expected3 };
+            yield return new object[] { input4, index4, expected4 };
+            yield return new object[] { input5, index5, expected5 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 02 Linked Lists/Task 02 Index from End/IndexFromEnd.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_02_Linked_Lists.Task_02_Index_from_End
+{
+    public class IndexFromEnd
+    {
+        public LinkedListNode<int> ElementByIndexFromEnd1(LinkedListNode<int> head, int index)
+        {
+            var count = 0;
+            var current = head;
+
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+
+            var indexFromStart = count - index - 1;
+            var node = head;
+
+            for (var i = 0; i < indexFromStart; i++)
+            {
+                node = node.Next;
+            }
+
+            return node;
+        }
+
+        public LinkedListNode<int> ElementByIndexFromEnd2(LinkedListNode<int> head, int index)
+        {
+            var current = head;
+            var orderedNodes = new List<LinkedListNode<int>>();
+
+            while (current != null)
+            {
+                orderedNodes.Add(current);
+                current = current.Next;
+            }
+
+            var indexFromStart = orderedNodes.Count - index - 1;
+            return orderedNodes[indexFromStart];
+        }
+    }
+}
