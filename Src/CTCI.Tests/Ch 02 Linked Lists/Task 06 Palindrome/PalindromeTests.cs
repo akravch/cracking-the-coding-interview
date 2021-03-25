c1617185c515fd33ca35a56a00519d15e643d190
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-25 17:39:37 +0300

    Add a solution for the palindrome.

---
+++ Src/CTCI.Tests/Ch 02 Linked Lists/Task 06 Palindrome/PalindromeTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_02_Linked_Lists.Task_06_Palindrome;
+using Xunit;
+
+namespace CTCI.Tests.Ch_02_Linked_Lists.Task_06_Palindrome
+{
+    public class PalindromeTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void IsPalindrome1(char[] input, bool expected)
+        {
+            var solution = new Palindrome();
+
+            var actual = solution.IsPalindrome1(LinkedListHelper.FromCollection(input));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void IsPalindrome2(char[] input, bool expected)
+        {
+            var solution = new Palindrome();
+
+            var actual = solution.IsPalindrome2(LinkedListHelper.FromCollection(input));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void IsPalindrome3(char[] input, bool expected)
+        {
+            var solution = new Palindrome();
+
+            var actual = solution.IsPalindrome3(LinkedListHelper.FromCollection(input));
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var input1 = new[] { 'a' };
+            var expected1 = true;
+
+            var input2 = new[] { 'a', 'a', 'a' };
+            var expected2 = true;
+
+            var input3 = new[] { 'a', 'a', 'b' };
+            var expected3 = false;
+
+            var input4 = new[] { 'b', 'a', 'a' };
+            var expected4 = false;
+
+            var input5 = new[] { 'b', 'a', 'a', 'c', 'a', 'a', 'b' };
+            var expected5 = true;
+
+            var input6 = new[] { 'b', 'c', 'a', 'a', 'c', 'b' };
+            var expected6 = true;
+
+            var input7 = new[] { 'a', 'a' };
+            var expected7 = true;
+
+            var input8 = new[] { 'a', 'b' };
+            var expected8 = false;
+
+            yield return new object[] { input1, expected1 };
+            yield return new object[] { input2, expected2 };
+            yield return new object[] { input3, expected3 };
+            yield return new object[] { input4, expected4 };
+            yield return new object[] { input5, expected5 };
+            yield return new object[] { input6, expected6 };
+            yield return new object[] { input7, expected7 };
+            yield return new object[] { input8, expected8 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 02 Linked Lists/Task 06 Palindrome/Palindrome.cs
@@ -0,0 +1,131 @@
+﻿namespace CTCI.Ch_02_Linked_Lists.Task_06_Palindrome
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome1(LinkedListNode<char> head)
+        {
+            LinkedListNode<char> reversedHead = null;
+            var current = head;
+            var length = 0;
+
+            while (current != null)
+            {
+                var newReversedHead = new LinkedListNode<char>(current.Value, reversedHead);
+                reversedHead = newReversedHead;
+                current = current.Next;
+                length++;
+            }
+
+            var currentForward = head;
+            var currentBack = reversedHead;
+            var currentIteration = 0;
+
+            while (currentForward != null && currentBack != null && currentIteration < length / 2)
+            {
+                if (currentForward.Value != currentBack.Value)
+                {
+                    return false;
+                }
+
+                currentForward = currentForward.Next;
+                currentBack = currentBack.Next;
+                currentIteration++;
+            }
+
+            return true;
+        }
+
+        public bool IsPalindrome2(LinkedListNode<char> head)
+        {
+            var length = GetLength(head);
+            var current = head;
+
+            for (var i = 0; i < length / 2; i++)
+            {
+                var currentFromEnd = GetElementAt(head, length - i - 1);
+
+                if (current.Value != currentFromEnd)
+                {
+                    return false;
+                }
+
+                current = current.Next;
+            }
+
+            return true;
+        }
+
+        public bool IsPalindrome3(LinkedListNode<char> head)
+        {
+            var length = GetLength(head);
+            var result =  IsPalindromeRecursive(head, length - 1);
+
+            return result.Success;
+        }
+
+        private static RecursiveResult IsPalindromeRecursive(LinkedListNode<char> head, int length)
+        {
+            if (length == 0)
+            {
+                return new RecursiveResult(true, head.Next);
+            }
+
+            if (length == 1)
+            {
+                return new RecursiveResult(head.Value == head.Next.Value, head.Next.Next);
+            }
+
+            var result = IsPalindromeRecursive(head.Next, length - 2);
+
+            if (!result.Success)
+            {
+                return new RecursiveResult(false, null);
+            }
+
+            if (head.Value != result.Node.Value)
+            {
+                return new RecursiveResult(false, null);
+            }
+
+            return new RecursiveResult(true, result.Node.Next);
+        }
+
+        private readonly struct RecursiveResult
+        {
+            public readonly bool Success;
+            public readonly LinkedListNode<char> Node;
+
+            public RecursiveResult(bool success, LinkedListNode<char> node)
+            {
+                Success = success;
+                Node = node;
+            }
+        }
+
+        private static int GetLength(LinkedListNode<char> head)
+        {
+            var length = 0;
+            var current = head;
+
+            while (current != null)
+            {
+                length++;
+                current = current.Next;
+            }
+
+            return length;
+        }
+
+        private static char GetElementAt(LinkedListNode<char> head, int index)
+        {
+            var current = head;
+
+            for (var i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            return current.Value;
+        }
+    }
+}
