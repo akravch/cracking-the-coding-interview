fdabda21306922ed9016470d4968dcd635412d09
Alexander Kravchuk <alexanderkr@playtika.com>
2021-03-26 17:09:29 +0300

    Add a solution for the intersection problem.

---
+++ Src/CTCI.Tests/Ch 02 Linked Lists/Task 07 Intersection/IntersectionTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_02_Linked_Lists.Task_07_Intersection;
+using Xunit;
+
+namespace CTCI.Tests.Ch_02_Linked_Lists.Task_07_Intersection
+{
+    public class IntersectionTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void GetIntersectionNode1(
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> firstHead,
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> secondHead,
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> expected)
+        {
+            var solution = new Intersection();
+
+            var actual = solution.GetIntersectionNode1(firstHead, secondHead);
+
+            Assert.Same(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void GetIntersectionNode2(
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> firstHead,
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> secondHead,
+            CTCI.Ch_02_Linked_Lists.LinkedListNode<int> expected)
+        {
+            var solution = new Intersection();
+
+            var actual = solution.GetIntersectionNode2(firstHead, secondHead);
+
+            Assert.Same(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var firstHead1 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(1);
+            var secondHead1 = firstHead1;
+            var expected1 = firstHead1;
+
+            var expected2 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(1,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(2));
+            var firstHead2 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(3,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(4, expected2));
+            var secondHead2 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(5, expected2);
+
+            var expected3 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(1,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(2,
+                    new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(3)));
+            var firstHead3 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(3,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(4,
+                    new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(5,
+                        expected3)));
+            var secondHead3 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(6, expected3);
+
+            var firstHead4 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(1,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(2));
+            var secondHead4 = new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(3,
+                new CTCI.Ch_02_Linked_Lists.LinkedListNode<int>(4));
+            var expected4 = (CTCI.Ch_02_Linked_Lists.LinkedListNode<int>) null;
+
+            yield return new object[] { firstHead1, secondHead1, expected1 };
+            yield return new object[] { firstHead2, secondHead2, expected2 };
+            yield return new object[] { firstHead3, secondHead3, expected3 };
+            yield return new object[] { firstHead4, secondHead4, expected4 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 02 Linked Lists/Task 07 Intersection/Intersection.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CTCI.Ch_02_Linked_Lists.Task_07_Intersection
+{
+    public class Intersection
+    {
+        public LinkedListNode<int> GetIntersectionNode1(LinkedListNode<int> firstHead, LinkedListNode<int> secondHead)
+        {
+            var firstResult = GetIterationResult(firstHead);
+            var secondResult = GetIterationResult(secondHead);
+
+            if (firstResult.Last != secondResult.Last)
+            {
+                return null;
+            }
+
+            LinkedListNode<int> longerHead;
+            LinkedListNode<int> shorterHead;
+
+            if (firstResult.Length > secondResult.Length)
+            {
+                longerHead = firstHead;
+                shorterHead = secondHead;
+            }
+            else
+            {
+                longerHead = secondHead;
+                shorterHead = firstHead;
+            }
+
+            var lengthDiff = Math.Abs(firstResult.Length - secondResult.Length);
+
+            var currentLonger = longerHead;
+            var currentShorter = shorterHead;
+
+            for (var i = 0; i < lengthDiff; i++)
+            {
+                currentLonger = currentLonger.Next;
+            }
+
+            while (currentLonger != currentShorter)
+            {
+                currentLonger = currentLonger.Next;
+                currentShorter = currentShorter.Next;
+            }
+
+            return currentLonger;
+        }
+
+        public LinkedListNode<int> GetIntersectionNode2(LinkedListNode<int> firstHead, LinkedListNode<int> secondHead)
+        {
+            if (firstHead == secondHead)
+            {
+                return firstHead;
+            }
+
+            var firstStack = ToStack(firstHead);
+            var secondStack = ToStack(secondHead);
+
+            if (firstStack.Peek() != secondStack.Peek())
+            {
+                return null;
+            }
+
+            var firstCurrentTail = firstStack.Pop();
+            var secondCurrentTail = secondStack.Pop();
+
+            if (firstCurrentTail != secondCurrentTail)
+            {
+                return null;
+            }
+
+            while (firstCurrentTail == secondCurrentTail &&
+                   firstStack.Count > 0 &&
+                   secondStack.Count > 0)
+            {
+                firstCurrentTail = firstStack.Pop();
+                secondCurrentTail = secondStack.Pop();
+            }
+
+            return firstCurrentTail.Next;
+        }
+
+        private static IterationResult GetIterationResult(LinkedListNode<int> head)
+        {
+            LinkedListNode<int> last = null;
+            var current = head;
+            var length = 0;
+
+            while (current != null)
+            {
+                length++;
+                last = current;
+                current = current.Next;
+            }
+
+            return new IterationResult(last, length);
+        }
+
+        private readonly struct IterationResult
+        {
+            public readonly LinkedListNode<int> Last;
+            public readonly int Length;
+
+            public IterationResult(LinkedListNode<int> last, int length)
+            {
+                Last = last;
+                Length = length;
+            }
+        }
+
+        private static Stack<LinkedListNode<int>> ToStack(LinkedListNode<int> head)
+        {
+            var stack = new Stack<LinkedListNode<int>>();
+            var current = head;
+
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.Next;
+            }
+
+            return stack;
+        }
+    }
+}
