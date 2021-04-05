af7f08f7e38eaadd1de32cdad761e3bd59044e27
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-05 12:29:05 +0300

    Implement stack sorting.

---
+++ Src/CTCI.Tests/Ch 03 Stacks and Queues/Task 05 Sort Stack/SortStackTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using CTCI.Ch_03_Stacks_and_Queues.Task_05_Sort_Stack;
+using Xunit;
+
+namespace CTCI.Tests.Ch_03_Stacks_and_Queues.Task_05_Sort_Stack
+{
+    public class SortStackTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void Sort1(int[] items)
+        {
+            var stack = StackFromArray(items);
+            var solution = new SortStack();
+            Array.Sort(items);
+
+            solution.Sort1(stack);
+
+            Assert.Equal(items, ArrayFromStack(stack));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void Sort2(int[] items)
+        {
+            var stack = StackFromArray(items);
+            var solution = new SortStack();
+            Array.Sort(items);
+
+            solution.Sort2(stack);
+
+            Assert.Equal(items, ArrayFromStack(stack));
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[] { new[] { 1, 3, 2, 4 } };
+            yield return new object[] { new[] { 5, 2, 5 } };
+            yield return new object[] { new[] { 1 } };
+            yield return new object[] { new[] { 1, 2 } };
+            yield return new object[] { new[] { 5, 2, 5, 2, 5, 2, 6, 2, 5, 2, 1, 6, 4, 6 } };
+        }
+
+        private static Stack<int> StackFromArray(int[] items)
+        {
+            var stack = new Stack<int>();
+
+            foreach (var item in items)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+
+        private static int[] ArrayFromStack(Stack<int> stack)
+        {
+            var array = new int[stack.Count];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = stack.Pop();
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Src/CTCI/Ch 03 Stacks and Queues/Task 05 Sort Stack/SortStack.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_03_Stacks_and_Queues.Task_05_Sort_Stack
+{
+    public class SortStack
+    {
+        public void Sort1(Stack<int> stack)
+        {
+            if (stack.Count < 2)
+            {
+                return;
+            }
+
+            var tempStack = new Stack<int>();
+
+            while (true)
+            {
+                var hadSwaps = false;
+
+                tempStack.Push(stack.Pop());
+
+                while (stack.Count > 0)
+                {
+                    var first = stack.Pop();
+                    var second = tempStack.Pop();
+
+                    if (first >= second)
+                    {
+                        tempStack.Push(second);
+                        tempStack.Push(first);
+                    }
+                    else
+                    {
+                        tempStack.Push(first);
+                        tempStack.Push(second);
+                        hadSwaps = true;
+                    }
+                }
+
+                // Can do the same backwards
+                while (tempStack.Count > 0)
+                {
+                    stack.Push(tempStack.Pop());
+                }
+
+                if (!hadSwaps)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void Sort2(Stack<int> stack)
+        {
+            var sortedStack = new Stack<int>();
+
+            while (stack.Count > 0)
+            {
+                var item = stack.Pop();
+
+                while (sortedStack.Count > 0)
+                {
+                    var sortedItem = sortedStack.Pop();
+
+                    if (sortedItem <= item)
+                    {
+                        sortedStack.Push(sortedItem);
+                        break;
+                    }
+
+                    stack.Push(sortedItem);
+                }
+
+                sortedStack.Push(item);
+            }
+
+            while (sortedStack.Count > 0)
+            {
+                stack.Push(sortedStack.Pop());
+            }
+        }
+    }
+}
