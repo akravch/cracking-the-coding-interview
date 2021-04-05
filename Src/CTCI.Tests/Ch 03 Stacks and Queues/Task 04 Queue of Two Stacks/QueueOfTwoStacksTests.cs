7769433b83c2c26ae699f52c1d459ede52af0288
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-05 11:27:21 +0300

    Add a solution for the queue of two stacks.

---
+++ Src/CTCI.Tests/Ch 03 Stacks and Queues/Task 04 Queue of Two Stacks/QueueOfTwoStacksTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_03_Stacks_and_Queues.Task_04_Queue_of_Two_Stacks;
+using Xunit;
+
+namespace CTCI.Tests.Ch_03_Stacks_and_Queues.Task_04_Queue_of_Two_Stacks
+{
+    public class QueueOfTwoStacksTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void QueueOfTwoStacks(int[] items)
+        {
+            var normalQueue = new Queue<int>();
+            var customQueue = new QueueOfTwoStacks();
+
+            foreach (var item in items)
+            {
+                normalQueue.Enqueue(item);
+                customQueue.Enqueue(item);
+
+                Assert.Equal(normalQueue.Count, customQueue.Count);
+            }
+
+            while (normalQueue.Count > 0)
+            {
+                Assert.Equal(normalQueue.Dequeue(), customQueue.Dequeue());
+                Assert.Equal(normalQueue.Count, customQueue.Count);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[] { new[] { 1 } };
+            yield return new object[] { new[] { 1, 2 } };
+            yield return new object[] { new[] { 5, 2, 5, 2, 5, 2, 6, 2, 5, 2, 1, 6, 4, 6 } };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 03 Stacks and Queues/Task 04 Queue of Two Stacks/QueueOfTwoStacks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_03_Stacks_and_Queues.Task_04_Queue_of_Two_Stacks
+{
+    public class QueueOfTwoStacks
+    {
+        private readonly Stack<int> _first = new();
+        private readonly Stack<int> _second = new();
+
+        public int Count => _first.Count + _second.Count;
+
+        public void Enqueue(int item)
+        {
+            while (_second.Count > 0)
+            {
+                _first.Push(_second.Pop());
+            }
+
+            _first.Push(item);
+        }
+
+        public int Dequeue()
+        {
+            while (_first.Count > 0)
+            {
+                _second.Push(_first.Pop());
+            }
+
+            return _second.Pop();
+        }
+    }
+}
