a529497c6218dd8b2da7df3d219a177105d9d44b
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-05 10:31:19 +0300

    Add a solution for finding min in a stack.

---
+++ Src/CTCI.Tests/Ch 03 Stacks and Queues/Task 02 Min/MinStackTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CTCI.Ch_03_Stacks_and_Queues.Task_02_Min;
+using Xunit;
+
+namespace CTCI.Tests.Ch_03_Stacks_and_Queues.Task_02_Min
+{
+    public class MinStackTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void MinStack(int[] array)
+        {
+            var minStack = new MinStack();
+            var normalStack = new Stack<int>();
+
+            foreach (var item in array)
+            {
+                minStack.Push(item);
+                normalStack.Push(item);
+
+                Assert.Equal(normalStack.Min(), minStack.Min);
+            }
+
+            while (minStack.Count > 1)
+            {
+                Assert.Equal(normalStack.Pop(), minStack.Pop());
+                Assert.Equal(normalStack.Min(), minStack.Min);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[] { new[] { 2 } };
+            yield return new object[] { new[] { 2, 2, 2, 2 } };
+            yield return new object[] { new[] { 4, 2, 5, 1, 9, 1 } };
+            yield return new object[] { new[] { 7, 4, 6, 1, 6, 9, 9, 5, 1, 5, 1, 1, 5, 2, 7 } };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 03 Stacks and Queues/Task 02 Min/MinStack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_03_Stacks_and_Queues.Task_02_Min
+{
+    public class MinStack
+    {
+        private readonly List<int> _items = new();
+        private readonly List<int> _minItems = new();
+
+        private int _headIndex = -1;
+        private int _minIndex = -1;
+
+        public void Push(int item)
+        {
+            _items.Add(item);
+            _headIndex++;
+
+            if (_minIndex == -1)
+            {
+                _minItems.Add(item);
+                _minIndex = 0;
+            }
+            else if (item <= _minItems[_minIndex])
+            {
+                _minItems.Add(item);
+                _minIndex++;
+            }
+        }
+
+        public int Pop()
+        {
+            var item = _items[_headIndex];
+
+            _items.RemoveAt(_headIndex);
+            _headIndex--;
+
+            if (item == _minItems[_minIndex])
+            {
+                _minItems.RemoveAt(_minIndex);
+                _minIndex--;
+            }
+
+            return item;
+        }
+
+        public int Count => _headIndex + 1;
+        public int Min => _minItems[_minIndex];
+    }
+}
