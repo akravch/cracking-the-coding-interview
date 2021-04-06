bb2d99761ad7da80598a62f75112ebd88c9cadbc
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-06 12:56:32 +0300

    Add a solution for building a binary search tree for an array.

---
+++ Src/CTCI.Tests/Ch 04 Trees/Task 02 Binary Search Tree for Array/BinarySearchTreeForArrayTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using CTCI.Ch_04_Trees;
+using CTCI.Ch_04_Trees.Task_02_Binary_Search_Tree_for_Array;
+using Xunit;
+
+namespace CTCI.Tests.Ch_04_Trees.Task_02_Binary_Search_Tree_for_Array
+{
+    public class BinarySearchTreeForArrayTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void Create(int[] array)
+        {
+            var solution = new BinarySearchTreeForArray();
+
+            var actual = solution.Create(array);
+
+            Assert.Equal(array, ArrayFromTree(actual));
+            Assert.True(IsBalanced(actual, array.Length));
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            yield return new object[]
+            {
+                new[] { 1, 2, 3, 4, 5, 6, 7 }
+            };
+            yield return new object[]
+            {
+                new[] { 2, 4, 6, 8, 10, 20 }
+            };
+            yield return new object[]
+            {
+                new[] { 1, 2, 3 }
+            };
+        }
+
+        private static int[] ArrayFromTree(BinaryTreeNode<int> root)
+        {
+            var list = new List<int>();
+            ListFromTreeRecursive(root, list);
+            return list.ToArray();
+
+            static void ListFromTreeRecursive(BinaryTreeNode<int> node, List<int> values)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                ListFromTreeRecursive(node.Left, values);
+                values.Add(node.Value);
+                ListFromTreeRecursive(node.Right, values);
+            }
+        }
+
+        private static int GetTreeLevelCount(BinaryTreeNode<int> root)
+        {
+            return GetLevelRecursive(root, 1);
+
+            static int GetLevelRecursive(BinaryTreeNode<int> node, int level)
+            {
+                if (node == null)
+                {
+                    return level - 1;
+                }
+
+                var leftSubtreeLevels = GetLevelRecursive(node.Left, level + 1);
+                var rightSubtreeLevels = GetLevelRecursive(node.Right, level + 1);
+
+                return Math.Max(leftSubtreeLevels, rightSubtreeLevels);
+            }
+        }
+
+        private static bool IsBalanced(BinaryTreeNode<int> root, int elementCount)
+        {
+            var levelCount = GetTreeLevelCount(root);
+
+            return
+                elementCount <= (int) Math.Pow(2, levelCount) - 1 &&
+                elementCount > (int) Math.Pow(2, levelCount - 1) - 1;
+
+        }
+    }
+}
---
+++ Src/CTCI/Ch 04 Trees/Task 02 Binary Search Tree for Array/BinarySearchTreeForArray.cs
@@ -0,0 +1,33 @@
+﻿namespace CTCI.Ch_04_Trees.Task_02_Binary_Search_Tree_for_Array
+{
+    public class BinarySearchTreeForArray
+    {
+        public BinaryTreeNode<int> Create(int[] array)
+        {
+            return CreateTreeRecursive(array, 0, array.Length - 1);
+        }
+
+        private static BinaryTreeNode<int> CreateTreeRecursive(
+            int[] array, int startIntervalIndex, int endIntervalIndex)
+        {
+            if (endIntervalIndex < startIntervalIndex)
+            {
+                return null;
+            }
+
+            var intervalRootIndex = (endIntervalIndex + startIntervalIndex) / 2;
+
+            var leftStartIntervalIndex = startIntervalIndex;
+            var leftEndIntervalIndex = intervalRootIndex - 1;
+
+            var rightStartIntervalIndex = intervalRootIndex + 1;
+            var rightEndIntervalIndex = endIntervalIndex;
+
+            return new BinaryTreeNode<int>(array[intervalRootIndex])
+            {
+                Left = CreateTreeRecursive(array, leftStartIntervalIndex, leftEndIntervalIndex),
+                Right = CreateTreeRecursive(array, rightStartIntervalIndex, rightEndIntervalIndex)
+            };
+        }
+    }
+}
