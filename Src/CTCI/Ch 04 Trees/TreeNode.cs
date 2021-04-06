91c74406400dc2543ecc9b63b4a5405cd380aebe
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-06 10:18:29 +0300

    Add tree node.

---
+++ Src/CTCI/Ch 04 Trees/TreeNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_04_Trees
+{
+    public class TreeNode<T>
+    {
+        public TreeNode()
+        {
+        }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public TreeNode(IEnumerable<TreeNode<T>> children)
+        {
+            Children.AddRange(children);
+        }
+
+        public TreeNode(T value, IEnumerable<TreeNode<T>> children)
+        {
+            Value = value;
+            Children.AddRange(children);
+        }
+
+        public T Value { get; set; }
+        public List<TreeNode<T>> Children { get; } = new();
+    }
+}
