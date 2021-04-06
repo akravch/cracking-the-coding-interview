61e02c9e03d3443cff133165e54310df3f1276ee
Alexander Kravchuk <alexanderkr@playtika.com>
2021-04-06 12:56:20 +0300

    Add BinaryTreeNode.

---
+++ Src/CTCI/Ch 04 Trees/BinaryTreeNode.cs
@@ -0,0 +1,18 @@
+﻿namespace CTCI.Ch_04_Trees
+{
+    public class BinaryTreeNode<T>
+    {
+        public BinaryTreeNode()
+        {
+        }
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+    }
+}
