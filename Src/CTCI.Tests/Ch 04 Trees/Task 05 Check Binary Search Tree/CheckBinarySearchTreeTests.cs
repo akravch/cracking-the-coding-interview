ad8473e858e03cd67e583a32f8c6b6d8d375c2b5
Alexander Kravchuk <alexanderkr@playtika.com>
2021-05-06 11:15:21 +0300

    Add a solution for checking whether a tree is a binary search tree.

---
+++ Src/CTCI.Tests/Ch 04 Trees/Task 05 Check Binary Search Tree/CheckBinarySearchTreeTests.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_04_Trees;
+using CTCI.Ch_04_Trees.Task_05_Check_Binary_Search_Tree;
+using Xunit;
+
+namespace CTCI.Tests.Ch_04_Trees.Task_05_Check_Binary_Search_Tree
+{
+    public class CheckBinarySearchTreeTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void IsBinarySearchTree1(BinaryTreeNode<int> root, bool expected)
+        {
+            var solution = new CheckBinarySearchTree();
+
+            var actual = solution.IsBinarySearchTree1(root);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void IsBinarySearchTree2(BinaryTreeNode<int> root, bool expected)
+        {
+            var solution = new CheckBinarySearchTree();
+
+            var actual = solution.IsBinarySearchTree2(root);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var root1 = new BinaryTreeNode<int>(4)
+            {
+                Left = new BinaryTreeNode<int>(2)
+                {
+                    Left = new BinaryTreeNode<int>(1),
+                    Right = new BinaryTreeNode<int>(3)
+                },
+                Right = new BinaryTreeNode<int>(6)
+                {
+                    Left = new BinaryTreeNode<int>(5),
+                    Right = new BinaryTreeNode<int>(7)
+                }
+            };
+            var expected1 = true;
+
+            var root2 = new BinaryTreeNode<int>(1)
+            {
+                Left = new BinaryTreeNode<int>(2)
+                {
+                    Left = new BinaryTreeNode<int>(4),
+                    Right = new BinaryTreeNode<int>(6)
+                },
+                Right = new BinaryTreeNode<int>(3)
+                {
+                    Left = new BinaryTreeNode<int>(5),
+                    Right = new BinaryTreeNode<int>(7)
+                }
+            };
+            var expected2 = false;
+
+            var root3 = new BinaryTreeNode<int>(4)
+            {
+                Left = new BinaryTreeNode<int>(3)
+                {
+                    Left = new BinaryTreeNode<int>(2)
+                    {
+                        Left = new BinaryTreeNode<int>(1)
+                    }
+                }
+            };
+            var expected3 = true;
+
+            var root4 = new BinaryTreeNode<int>(42);
+            var expected4 = true;
+
+            var root5 = new BinaryTreeNode<int>(4)
+            {
+                Left = new BinaryTreeNode<int>(2)
+                {
+                    Left = new BinaryTreeNode<int>(1),
+                    Right = new BinaryTreeNode<int>(3)
+                }
+            };
+            var expected5 = true;
+
+            var root6 = new BinaryTreeNode<int>(4)
+            {
+                Left = new BinaryTreeNode<int>(3)
+                {
+                    Left = new BinaryTreeNode<int>(2)
+                    {
+                        Left = new BinaryTreeNode<int>(1)
+                    }
+                },
+                Right = new BinaryTreeNode<int>(5)
+                {
+                    Right = new BinaryTreeNode<int>(6)
+                }
+            };
+            var expected6 = true;
+
+            var root7 = new BinaryTreeNode<int>(20)
+            {
+                Left = new BinaryTreeNode<int>(10)
+                {
+                    Right = new BinaryTreeNode<int>(25)
+                },
+                Right = new BinaryTreeNode<int>(30)
+            };
+            var expected7 = false;
+
+            yield return new object[] { root1, expected1 };
+            yield return new object[] { root2, expected2 };
+            yield return new object[] { root3, expected3 };
+            yield return new object[] { root4, expected4 };
+            yield return new object[] { root5, expected5 };
+            yield return new object[] { root6, expected6 };
+            yield return new object[] { root7, expected7 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 04 Trees/Task 05 Check Binary Search Tree/CheckBinarySearchTree.cs
@@ -0,0 +1,115 @@
+﻿namespace CTCI.Ch_04_Trees.Task_05_Check_Binary_Search_Tree
+{
+    public class CheckBinarySearchTree
+    {
+        public bool IsBinarySearchTree1(BinaryTreeNode<int> root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            var pair = IsBinarySearchTree1Internal(root);
+
+            return pair.IsBinarySearchTree;
+        }
+
+        public bool IsBinarySearchTree2(BinaryTreeNode<int> root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return IsBinarySearchTree2Internal(root, int.MinValue, int.MaxValue);
+        }
+
+        private readonly struct Pair
+        {
+            public readonly bool IsBinarySearchTree;
+            public readonly BinaryTreeNode<int> LastPrinted;
+
+            public Pair(bool isBinarySearchTree, BinaryTreeNode<int> lastPrinted)
+            {
+                IsBinarySearchTree = isBinarySearchTree;
+                LastPrinted = lastPrinted;
+            }
+        }
+
+        private static Pair IsBinarySearchTree1Internal(BinaryTreeNode<int> node)
+        {
+            Pair leftPair;
+
+            if (node.Left != null)
+            {
+                leftPair = IsBinarySearchTree1Internal(node.Left);
+
+                if (!leftPair.IsBinarySearchTree)
+                {
+                    return new Pair(false, null);
+                }
+            }
+            else
+            {
+                leftPair = new Pair(true, null);
+            }
+
+            Pair rightPair;
+
+            if (node.Right != null)
+            {
+                rightPair = IsBinarySearchTree1Internal(node.Right);
+
+                if (!rightPair.IsBinarySearchTree)
+                {
+                    return new Pair(false, null);
+                }
+            }
+            else
+            {
+                rightPair = new Pair(true, null);
+            }
+
+            if (leftPair.LastPrinted != null && leftPair.LastPrinted.Value > node.Value)
+            {
+                return new Pair(false, null);
+            }
+
+            if (rightPair.LastPrinted != null && rightPair.LastPrinted.Value <= node.Value)
+            {
+                return new Pair(false, null);
+            }
+
+            return new Pair(true, rightPair.LastPrinted ?? node);
+        }
+
+        private bool IsBinarySearchTree2Internal(BinaryTreeNode<int> node, int min, int max)
+        {
+            var left = node.Left;
+
+            if (left != null)
+            {
+                if (left.Value > node.Value)
+                {
+                    return false;
+                }
+
+                return IsBinarySearchTree2Internal(left, min, left.Value);
+            }
+
+            var right = node.Right;
+
+            if (right != null)
+            {
+                if (right.Value <= node.Value)
+                {
+                    return false;
+                }
+
+                return IsBinarySearchTree2Internal(right, right.Value, max);
+            }
+
+            return node.Value > min && node.Value <= max;
+        }
+    }
+}
