18e2dcba52fa55603fe3b3081859f5a0a7d85663
Alexander Kravchuk <alexanderkr@playtika.com>
2021-05-19 17:06:42 +0300

    Add a solution for project build.

---
+++ Src/CTCI.Tests/Ch 04 Trees/Task 07 Project Dependencies/ProjectDependenciesTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using CTCI.Ch_04_Trees.Task_07_Project_Dependencies;
+using Xunit;
+
+namespace CTCI.Tests.Ch_04_Trees.Task_07_Project_Dependencies
+{
+    public class ProjectDependenciesTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void GetBuildOrder(string[] projects, (string, string)[] dependencies, string[] expected)
+        {
+            var solution = new ProjectDependencies();
+
+            var actual = solution.GetBuildOrder(projects, dependencies);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var projects1 = new[] { "a", "b", "c", "d", "e", "f" };
+            var dependencies1 = new[] { ("d", "a"), ("b", "f"), ("d", "b"), ("a", "f"), ("c", "d") };
+            var order1 = new[] { "f", "a", "b", "d", "c", "e" };
+
+            var projects2 = new[] { "a", "b", "c", "d", "e", "f" };
+            var dependencies2 = new[] { ("d", "a"), ("b", "f"), ("d", "b"), ("a", "f"), ("c", "d"), ("f", "d") };
+            string[] order2 = null;
+
+            var projects3 = new[] { "a", "b", "c", "d", "e", "f" };
+            var dependencies3 = new[] { ("a", "b"), ("b", "c"), ("c", "d"), ("d", "e"), ("e", "f") };
+            var order3 = new[] { "f", "e", "d", "c", "b", "a" };
+
+            yield return new object[] { projects1, dependencies1, order1 };
+            yield return new object[] { projects2, dependencies2, order2 };
+            yield return new object[] { projects3, dependencies3, order3 };
+        }
+    }
+}
---
+++ Src/CTCI/Ch 04 Trees/Task 07 Project Dependencies/ProjectDependencies.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace CTCI.Ch_04_Trees.Task_07_Project_Dependencies
+{
+    public class ProjectDependencies
+    {
+        private sealed class ProjectNode
+        {
+            public ProjectNode(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; }
+            public bool Built { get; set; }
+            public List<ProjectNode> Dependencies { get; } = new();
+            public List<ProjectNode> Dependent { get; } = new();
+        }
+
+        public IList<string> GetBuildOrder(IList<string> projects, IList<(string, string)> dependencies)
+        {
+            var projectNodes = new Dictionary<string, ProjectNode>();
+
+            foreach (var project in projects)
+            {
+                projectNodes[project] = new ProjectNode(project);
+            }
+
+            foreach (var (project, dependency) in dependencies)
+            {
+                var projectNode = projectNodes[project];
+                var dependencyNode = projectNodes[dependency];
+
+                dependencyNode.Dependent.Add(projectNode);
+                projectNode.Dependencies.Add(dependencyNode);
+            }
+
+            var order = new List<string>(projects.Count);
+
+            foreach (var node in projectNodes.Values)
+            {
+                if (node.Dependent.Count == 0)
+                {
+                    var success = Traverse(node, new HashSet<ProjectNode>(), order);
+
+                    if (!success)
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return order;
+        }
+
+        private static bool Traverse(ProjectNode node, HashSet<ProjectNode> visitedNodes, List<string> order)
+        {
+            if (node.Built)
+            {
+                return true;
+            }
+
+            if (!visitedNodes.Add(node))
+            {
+                return false;
+            }
+
+            foreach (var child in node.Dependencies)
+            {
+                if (!Traverse(child, visitedNodes, order))
+                {
+                    return false;
+                }
+            }
+
+            node.Built = true;
+            order.Add(node.Name);
+
+            return true;
+        }
+    }
+}
